814348b8843749357e14fd12079b064d4bb22f17
RafiuddinBhuyan <rafiuddinbhuyan8@gmail.com>
2026-07-05 09:45:00 +0600

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -21,12 +21,12 @@ int main()
 {
     char ch;
     double a, b;
-    cout<<"some rule:"<<endl;
-    cout<<"for add, char = + ;"<<endl;
-    cout<<"for sub, char = - ;"<<endl;
-    cout<<"for mult, char = * ;"<<endl;
-    cout<<"for div, char = / ;"<<endl;
-    cout<<"for exit, char = x ;"<<endl;
+    cout<<"=====SOME RULE====="<<endl;
+    cout<<"For add, char = + ;"<<endl;
+    cout<<"For sub, char = - ;"<<endl;
+    cout<<"For mult, char = * ;"<<endl;
+    cout<<"For div, char = / ;"<<endl;
+    cout<<"For exit, char = x ;"<<endl;
     while (1)
     {
         cout << "enter your char" << endl;
