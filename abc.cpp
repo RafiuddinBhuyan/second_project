335f59225ea41827d5275a5704eb1d85a9ac2eae
RafiuddinBhuyan <rafiuddinbhuyan8@gmail.com>
2026-07-05 09:14:06 +0600

    flat folder structure with all files

---
+++ abc.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"abcd";
+    cout<<"efg";
+    cout<<"ok its finr";
+    return 0;
+}
---
+++ calculator.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+double add(double a, double b)
+{
+    return a + b;
+}
+double sub(double a, double b)
+{
+    return a - b;
+}
+double mult(double a, double b)
+{
+    return a * b;
+}
+double div(double a, double b)
+{
+    return a / b;
+}
+
+int main()
+{
+    char ch;
+    double a, b;
+    while (1)
+    {
+        cout << "enter your char" << endl;
+        cin >> ch;
+        if (ch == '+')
+        {
+            cout << "enter your tow element" << endl;
+            cin >> a >> b;
+            cout << add(a, b) << endl;
+        }
+        else if (ch == '-')
+        {
+            cout << "enter your tow element" << endl;
+            cin >> a >> b;
+            cout << sub(a, b) << endl;
+        }
+        else if (ch == '*')
+        {
+            cout << "enter your tow element" << endl;
+            cin >> a >> b;
+            cout << mult(a, b) << endl;
+        }
+        else if (ch == '/')
+        {
+            cout << "enter your tow element" << endl;
+            cin >> a >> b;
+            cout << div(a, b) << endl;
+        }
+        else if (ch == 'x')
+        {
+            return 0;
+        }
+    }
+    return 0;
+}
